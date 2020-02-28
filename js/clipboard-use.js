f6582fbcbcd70624bf0983a49cece3a4a98db2ba
microlong666 <1095289759@qq.com>
2020-02-28 17:57:30 +0800

    Site updated: 2020-02-28 17:57:29

--- js/clipboard-use.js
+++ js/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 28 2020 17:51:31 GMT+0800 (中国标准时间)
-!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code").before(n);new Clipboard(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
+// build time:Fri Feb 28 2020 17:57:29 GMT+0800 (中国标准时间)
+!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
